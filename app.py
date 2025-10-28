26e65a8e2c8ffffe50f51c3b276df1dc60967758
Hemanth Narravula <hemanthnarravula12@gmail.com>
2025-10-28 12:06:17 +0530

    Fix port binding for Render deployment

--- app.py
+++ app.py
@@ -1063,4 +1063,6 @@ def reset_password():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    # Use PORT environment variable for Render deployment
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
